18eed879927a05e4ac7e4a51e8bbaf70af26e35d
gtmaihackathon <vipin.kumar@productsup.com>
2025-11-27 11:41:19 +0100

    Update vector_db.py

--- app/vector_db.py
+++ app/vector_db.py
@@ -1,1 +1,16 @@
+import faiss, numpy as np
+
+class FAISSIndex:
+    def __init__(self, dim):
+        self.index = faiss.IndexFlatL2(dim)
+        self.asset_map = []  # map idx -> asset_id
+
+    def add(self, vectors, asset_ids):
+        self.index.add(vectors)
+        self.asset_map.extend(asset_ids)
+
+    def search(self, query_vector, top_k=5):
+        D, I = self.index.search(np.array([query_vector]), top_k)
+        results = [(self.asset_map[i], float(D[0][j])) for j, i in enumerate(I[0])]
+        return results
 
